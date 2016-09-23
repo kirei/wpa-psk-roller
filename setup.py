1f375b005f2ff8ecc4efbdadd4f34e9133073d28
Jakob Schlyter <jakob@kirei.se>
2016-09-23 11:05:53 +0200

    py3

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ setup(
     description='WPA PSK Roller for ArubaPS',
     author='Kirei AB',
     author_email='info@kirei.se',
-    classifiers=['License :: OSI Approved :: BSD License'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
     url='https://github.com/kirei/wpa-psk-rollover',
     scripts=['wpa_psk_roller.py'],
     install_requires=[
